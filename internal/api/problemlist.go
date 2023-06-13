dbe12ab8daa7ac994651f1d1284fe4296be7f8f0
Andres <afmc07@hotmail.com>
2023-06-13 18:10:28 -0300

    Changed max on Page_size

--- internal/api/problemlist.go
+++ internal/api/problemlist.go
@@ -49,7 +49,7 @@ func (server *Server) getProblemList(ctx *gin.Context) {
 
 type listProblemListsRequest struct {
 	PageID   int32 `form:"page_id" binding:"required,min=1"`
-	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=30"`
 }
 
 func (server *Server) listProblemLists(ctx *gin.Context) {
